a56336cdb553b62c4fa6bb789afe1aa08f987aab
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-08-26 11:49:09 -0300

    Update and rename AdvinhaçãocomWhile.py to AdvinhaçãocomLoop.py

--- src/Curso1/AdvinhaçãocomLoop.py
+++ src/Curso1/AdvinhaçãocomLoop.py
@@ -5,24 +5,27 @@ print("**************************")
 total_tentativas = 3
 i = 0
 num_aletorio = 42
-while(i < total_tentativas):
+for i in range(0,total_tentativas):
     chute = input("Qual seu Chute")
-    print("Vc digitou:",chute)
-    int_chute = int(chute )
-
+    print("digite um numero entre 1 e 100:",chute)
+    int_chute = int(chute)
+    if(int_chute > 100 or int_chute < 0):
+        print("Vc deve digitar um Numero entre 1 e 100")
+        continue
     if(int_chute == num_aletorio):
 
         print("Igual")
-
+        break
     else:
        if(int_chute >= num_aletorio):
            print("Maior ou igual")
        elif(int_chute < num_aletorio):
            print("Menor")
 
-    print("Tentativa de numero: ",i+1)
-    i = i + 1
+    print("Tentativa de Nª{}: ".format(i+1))
 
 
 
-print("Endgame")
+
+print("ENDGAME")
+
