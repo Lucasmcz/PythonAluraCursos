60df621fc1bfb234283acf00ce04594df1da5e95
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-08-26 11:01:03 -0300

    Create Advinhação.py

---
+++ src/Curso1/Advinhação.py
@@ -0,0 +1,18 @@
+print("**************************")
+print("Welcome to Advinhação game")
+print("**************************")
+
+
+num_aletorio = 42
+chute = input("Qual seu Chute")
+print("Vc digitou:",chute)
+int_chute = int(chute )
+if(int_chute == num_aletorio):
+
+    print("Igual")
+
+
+else:
+    print("Não é igual ")
+
+print("Endgame")
